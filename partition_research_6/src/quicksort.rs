a4944e8dbba5f53648687005faf57189e08ff7ed
Deividas Zaleskis <rr612788+DFIN@fincoad.com>
2024-12-28 23:44:41 +0200

    add unoptimized block lomuto

--- partition_research_6/src/quicksort.rs
+++ partition_research_6/src/quicksort.rs
@@ -1,6 +1,6 @@
 use core::cmp;
 use core::intrinsics;
-use core::mem::{self, ManuallyDrop};
+use core::mem;
 use core::ptr;
 
 use crate::heapsort;
@@ -140,7 +140,7 @@ where
 
     // This construct is used to limit the LLVM IR generated, which saves large amounts of
     // compile-time by only instantiating the code that is needed. Idea by Frank Steffahn.
-    let num_lt = partition_lomuto_branchless_cyclic(v_without_pivot, pivot, is_less);
+    let num_lt = partition_lomuto_block(v_without_pivot, pivot, is_less);
 
     // Place the pivot between the two partitions.
     v.swap(0, num_lt);
@@ -148,141 +148,41 @@ where
     num_lt
 }
 
-fn partition_lomuto_branchless_cyclic<T, F>(v: &mut [T], pivot: &T, is_less: &mut F) -> usize
+fn partition_lomuto_block<T, F>(v: &mut [T], pivot: &T, is_less: &mut F) -> usize
 where
     F: FnMut(&T, &T) -> bool,
 {
-    // Novel partition implementation by Lukas Bergdoll and Orson Peters. Branchless Lomuto
-    // partition paired with a cyclic permutation. TODO link writeup.
+    // Number of elements in a typical block.
+    const BLOCK_SIZE: usize = 128;
+    let mut block = [0; BLOCK_SIZE];
 
     let len = v.len();
     let v_base = v.as_mut_ptr();
 
-    if len == 0 {
-        return 0;
-    }
-
-    // SAFETY: We checked that `len` is more than zero, which means that reading `v_base` is safe to
-    // do. From there we have a bounded loop where `v_base.add(i)` is guaranteed in-bounds. `v` and
-    // `pivot` can't alias because of type system rules. The drop-guard `gap` ensures that should
-    // `is_less` panic we always overwrite the duplicate in the input. `gap.pos` stores the previous
-    // value of `right` and starts at `v_base` and so it too is in-bounds. Given `UNROLL_LEN == 2`
-    // after the main loop we either have A) the last element in `v` that has not yet been processed
-    // because `len % 2 != 0`, or B) all elements have been processed except the gap value that was
-    // saved at the beginning with `ptr::read(v_base)`. In the case A) the loop will iterate twice,
-    // first performing loop_body to take care of the last element that didn't fit into the unroll.
-    // After that the behavior is the same as for B) where we use the saved value as `right` to
-    // overwrite the duplicate. If this very last call to `is_less` panics the saved value will be
-    // copied back including all possible changes via interior mutability. If `is_less` does not
-    // panic and the code continues we overwrite the duplicate and do `right = right.add(1)`, this
-    // is safe to do with `&mut *gap.value` because `T` is the same as `[T; 1]` and generating a
-    // pointer one past the allocation is safe.
     unsafe {
-        let mut loop_body = |state: &mut PartitionState<T>| {
-            let right_is_lt = is_less(&*state.right, pivot);
-            let left = v_base.add(state.num_lt);
+        let mut start = 0;
+        let mut left = v_base;
 
-            ptr::copy(left, state.gap.pos, 1);
-            ptr::copy_nonoverlapping(state.right, left, 1);
+        while start < len {
+            let rem = len - start;
+            let to_fill = cmp::min(BLOCK_SIZE, rem);
+            let mut num = 0;
 
-            state.gap.pos = state.right;
-            state.num_lt += right_is_lt as usize;
-
-            state.right = state.right.add(1);
-        };
-
-        // Ideally we could just use GapGuard in PartitionState, but the reference that is
-        // materialized with `&mut state` when calling `loop_body` would create a mutable reference
-        // to the parent struct that contains the gap value, invalidating the reference pointer
-        // created from a reference to the gap value in the cleanup loop. This is only an issue
-        // under Stacked Borrows, Tree Borrows accepts the intuitive code using GapGuard as valid.
-        let mut gap_value = ManuallyDrop::new(ptr::read(v_base));
-
-        let mut state = PartitionState {
-            num_lt: 0,
-            right: v_base.add(1),
-
-            gap: GapGuardRaw {
-                pos: v_base,
-                value: &mut *gap_value,
-            },
-        };
-
-        // Manual unrolling that works well on x86, Arm and with opt-level=s without murdering
-        // compile-times. Leaving this to the compiler yields ok to bad results.
-        let unroll_len = if const { mem::size_of::<T>() <= 16 } {
-            2
-        } else {
-            1
-        };
-
-        let unroll_end = v_base.add(len - (unroll_len - 1));
-        while state.right < unroll_end {
-            if unroll_len == 2 {
-                loop_body(&mut state);
-                loop_body(&mut state);
-            } else {
-                loop_body(&mut state);
+            for j in 0..to_fill {
+                let right = v_base.add(start + j);
+                block[num] = j;
+                num += is_less(&*right, pivot) as usize;
             }
-        }
 
-        // Single instantiate `loop_body` for both the unroll cleanup and cyclic permutation
-        // cleanup. Optimizes binary-size and compile-time.
-        let end = v_base.add(len);
-        loop {
-            let is_done = state.right == end;
-            state.right = if is_done {
-                state.gap.value
-            } else {
-                state.right
-            };
-
-            loop_body(&mut state);
-
-            if is_done {
-                mem::forget(state.gap);
-                break;
+            for j in 0..num {
+                let right = v_base.add(start + block[j]);
+                ptr::swap(left, right);
+                left = left.add(1);
             }
+
+            start += to_fill;
         }
 
-        state.num_lt
+        left.sub_ptr(v_base)
     }
-}
-
-struct GapGuard<T> {
-    pos: *mut T,
-    value: ManuallyDrop<T>,
-}
-
-impl<T> Drop for GapGuard<T> {
-    fn drop(&mut self) {
-        unsafe {
-            ptr::copy_nonoverlapping(&*self.value, self.pos, 1);
-        }
-    }
-}
-
-/// Ideally this wouldn't be needed and we could just use the regular GapGuard.
-/// See comment in [`partition_lomuto_branchless_cyclic`].
-struct GapGuardRaw<T> {
-    pos: *mut T,
-    value: *mut T,
-}
-
-impl<T> Drop for GapGuardRaw<T> {
-    fn drop(&mut self) {
-        unsafe {
-            ptr::copy_nonoverlapping(self.value, self.pos, 1);
-        }
-    }
-}
-
-struct PartitionState<T> {
-    // The current element that is being looked at, scans left to right through slice.
-    right: *mut T,
-    // Counts the number of elements that compared less-than, also works around:
-    // https://github.com/rust-lang/rust/issues/117128
-    num_lt: usize,
-    // Gap guard that tracks the temporary duplicate in the input.
-    gap: GapGuardRaw<T>,
 }
